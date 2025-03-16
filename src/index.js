94cbf1c8c863d7878d50505788b21d6d853b73bc
Miki22986 <elisa.miki@gmail.com>
2025-03-16 23:25:25 +0100

    codice non funzionante

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
-let city = document.querySelector(".NameCity");
-let data = document.querySelector(".data");
-let hour = document.querySelector(".ora");
+let roma = document.querySelector("#roma");
+let data = roma.querySelector(".data");
+let hour = roma.querySelector(".ora");
+let city_rome = moment().tz("Europe/Rome");
+
+data.innerHTML = city_rome.format("MMM dddd Do YY");
+hour.innerHTML = city_rome.format('LTS');
