69567b138f36fc4af5dbd118d582120a5ff679bb
Miki22986 <elisa.miki@gmail.com>
2025-03-16 23:35:39 +0100

    aggiornato codice

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 let roma = document.querySelector("#roma");
 let data = roma.querySelector(".data");
-let hour = roma.querySelector(".ora");
+let ora = roma.querySelector(".ora");
 let city_rome = moment().tz("Europe/Rome");
 
-data.innerHTML = city_rome.format("MMM dddd Do YY");
-hour.innerHTML = city_rome.format('LTS');
+data.innerHTML = city_rome.format("MMMM	Do YYYY");
+ora.innerHTML = city_rome.format("h:mm:ss [<small>]A[</small>]");
