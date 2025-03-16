588ed50167366462604642116daef9eeb8c869a6
Miki22986 <elisa.miki@gmail.com>
2025-03-17 00:09:37 +0100

    codice funzionante

--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
+setInterval(function(){
+    let RomeElement = document.querySelector("#rome");
+    let RomeData = RomeElement.querySelector(".data");
+    let ora = RomeElement.querySelector(".ora");
+    let Rome = moment().tz("Europe/Rome");
 
-let data = document.querySelector("#dataRoma");
-let ora = document.querySelector("#oraRoma");
-let Rome = moment().tz("Europe/Rome");
+    RomeData.innerHTML= Rome.format("MMMM Do YYYY");
+    ora.innerHTML = Rome.format("h:mm:ss [<small>]A[</small>]");
+});
 
- data.innerHTML= Rome.format("MMMM	Do YYYY");
- ora.innerHTML = Rome.format("h:mm:ss [<small>]A[</small>]");
+setInterval(function(){
+    let losAngelesElement = document.querySelector("#los-angeles");
+    let losAngelesDateElement = losAngelesElement.querySelector(".data");
+    let losAngelesTimeElement = losAngelesElement.querySelector(".ora");
+    let losAngelesTime = moment().tz("America/Los_Angeles");
 
+    losAngelesDateElement.innerHTML = losAngelesTime.format("MMMM Do YYYY");
+    losAngelesTimeElement.innerHTML = losAngelesTime.format("h:mm:ss [<small>]A[</small>]" );
+});
